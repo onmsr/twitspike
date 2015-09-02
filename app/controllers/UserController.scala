a370bafc86c5b92420c2f83f778de40f58a72ca6
onuma <masaru_onuma@dwango.co.jp>
2015-09-02 17:18:10 +0900

    ユーザー登録・削除・取得・認証・認証ユーザーの取得のAPIを追加

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,24 +1,165 @@
 package jp.co.dwango.twitspike.controllers
 
-import play.api.mvc.Controller
+import jp.co.dwango.twitspike.models.User
+import jp.co.dwango.twitspike.models.response.UserResponseData
+import jp.co.dwango.twitspike.services.AerospikeService
+import jp.co.dwango.twitspike.services.UserService
+import jp.co.dwango.twitspike.validations.UserRequestDataConstraint
+import jp.co.dwango.twitspike.exceptions.TwitSpikeException
+import jp.co.dwango.twitspike.actions.UserAction
+import play.api.data.Form
+import play.api.libs.json.Json
+import play.api.mvc.Action
+import scala.util.control.Exception.allCatch
+import jp.co.dwango.twitspike.exceptions.TwitSpikeException.writes
+import jp.co.dwango.twitspike.exceptions.TwitSpikeExceptionTrait
 
 /**
  * UserController
- * 
+ *
  * ユーザー関連のAPIを管理するコントローラー
  */
-class UserController extends BaseController {
+class UserController extends BaseController with TwitSpikeExceptionTrait {
 
   def index = TODO
-  def post = TODO
-  def delete(userId: String) = TODO
-  def get(userId: String) = TODO
+
+  def getRequestData[T](form: Form[T])(implicit request: play.api.mvc.Request[_]): Either[Exception, T] = {
+    val param = form.bindFromRequest
+    if (param.hasErrors || param.hasGlobalErrors) {
+      Left(new TwitSpikeException(TwitSpikeException.VALIDATIONS_ERROR, validationErrorMessage))
+    } else {
+      Right(param.value.get)
+    }
+  }
+
+  def post = Action(parse.json) { implicit request =>
+    (for {
+      client <- Right(AerospikeService.getClient).right
+      u <- getRequestData(UserRequestDataConstraint.post).right
+      id <- new UserService(client).create(u.name, u.nickname, u.email, u.description, u.rawPassword).right
+      _ <- (allCatch either client.close).right
+    } yield id) match {
+      case Left(e) => {
+        e match {
+          case TwitSpikeException(VALIDATIONS_ERROR, _) => BadRequest(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case _ => InternalServerError(Json.obj("error" -> internalServerErrorMessage))
+        }
+      }
+      case Right(id) => Created(Json.obj("id" -> id))
+    }
+  }
+
+  def delete(userId: Long) = UserAction { implicit request =>
+    (for {
+      sessionKey <- request.sessionKey.toRight(
+        new TwitSpikeException(SESSION_KEY_REQUIRED_ERROR, sessionKeyRequiredErrorMessage)).right
+      user <- request.user.toRight(
+        new TwitSpikeException(AUTH_USER_NOT_FOUND_ERROR, userNotFoundErrorMessage)).right
+    } yield user) match {
+      case Left(e) => {
+        e match {
+          case TwitSpikeException(SESSION_KEY_REQUIRED_ERROR, _) => BadRequest(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case TwitSpikeException(AUTH_USER_NOT_FOUND_ERROR, _) => NotFound(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case _ => InternalServerError(Json.obj("error" -> internalServerErrorMessage))
+        }
+      }
+      case Right(user) => {
+        // 削除する権限があるかどうか確認
+        if (user.id == userId) {
+          (for {
+            client <- Right(AerospikeService.getClient).right
+            result <- new UserService(client).delete(userId).right
+            _ <- (allCatch either client.close).right
+          } yield result) match {
+            case Left(e) => InternalServerError(Json.obj("error" -> internalServerErrorMessage))
+            case Right(result) => NoContent
+          }
+        } else {
+          Forbidden(Json.obj("error" -> permissionErrorMessage))
+        }
+      }
+    }
+  }
+
+  def get(userId: Long) = Action { implicit request =>
+    (for {
+      client <- Right(AerospikeService.getClient).right
+      user <- new UserService(client).findOneById(userId).toRight(
+        new TwitSpikeException(USER_NOT_FOUND_ERROR, userNotFoundErrorMessage)).right
+      userResponse <- Right(getUserResponseData(user)).right
+      _ <- (allCatch either client.close).right
+    } yield userResponse) match {
+      case Left(e) => {
+        e match {
+          case TwitSpikeException(USER_NOT_FOUND_ERROR, _) => NotFound(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case _ => InternalServerError(Json.obj("error" -> internalServerErrorMessage))
+        }
+      }
+      case Right(user) => {
+        import UserResponseData.writes
+        Ok(Json.obj("user" -> Json.toJson(user)))
+      }
+    }
+  }
+
+  private[this] def getUserResponseData[A](u: User) = {
+    UserResponseData(
+      u.id,
+      u.nickname,
+      u.nickname,
+      u.email,
+      u.description,
+      None,
+      None
+    )
+  }
+
   def list() = TODO
-  def auth() = TODO
-  def self() = TODO
-  def fans(userId: String) = TODO
-  def celebs(userId: String) = TODO
-  def tweets(userId: String) = TODO
-  def timeline(userId: String) = TODO
+
+  def auth() = Action(parse.json) { implicit request =>
+    (for {
+      client <- Right(AerospikeService.getClient).right
+      u <- getRequestData(UserRequestDataConstraint.auth).right
+      sessionKey <- new UserService(client).auth(u.email, u.rawPassword).right
+      _ <- (allCatch either client.close).right
+    } yield sessionKey) match {
+      case Left(e) => {
+        e match {
+          case TwitSpikeException(VALIDATIONS_ERROR, _) => BadRequest(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case TwitSpikeException(AUTH_FAILED_ERROR, _) => Unauthorized(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case TwitSpikeException(AUTH_USER_NOT_FOUND_ERROR, _) => NotFound(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case _ => InternalServerError(Json.obj("error" -> internalServerErrorMessage))
+        }
+      }
+      case Right(sessionKey) => Ok(Json.obj("session_key" -> sessionKey))
+    }
+  }
+
+  def self() = UserAction { implicit request =>
+    (for {
+      sessionKey <- request.sessionKey.toRight(
+        new TwitSpikeException(SESSION_KEY_REQUIRED_ERROR, sessionKeyRequiredErrorMessage)).right
+      u <- request.user.toRight(
+        new TwitSpikeException(AUTH_USER_NOT_FOUND_ERROR, userNotFoundErrorMessage)).right
+      user <- Right(getUserResponseData(u)).right
+    } yield user) match {
+      case Left(e) => {
+        e match {
+          case TwitSpikeException(SESSION_KEY_REQUIRED_ERROR, _) => BadRequest(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case TwitSpikeException(AUTH_USER_NOT_FOUND_ERROR, _) => NotFound(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case _ => InternalServerError(Json.obj("error" -> internalServerErrorMessage))
+        }
+      }
+      case Right(user) => {
+        import UserResponseData.writes
+        Ok(Json.obj("user" -> user))
+      }
+    }
+  }
+
+  def fans(userId: Long) = TODO
+  def celebs(userId: Long) = TODO
+  def tweets(userId: Long) = TODO
+  def timeline(userId: Long) = TODO
 
 }
