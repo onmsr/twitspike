8b4185e85d121a87a182fd0d41a4b5f16796c26f
onuma <masaru_onuma@dwango.co.jp>
2015-09-03 16:33:23 +0900

    ツイートの登録と取得のテスト追加

--- test/TweetAPISpec.scala
+++ test/TweetAPISpec.scala
@@ -33,8 +33,37 @@ class TweetAPISpec extends Specification {
         contentType(fres) must beSome("application/json")
         contentAsString(fres) must contain ("id")
       }
+    }
+
+    // "[DELETE /tweets/:tweetId] ツイートの削除" should {}
+    
+    "[GET /tweets/:tweetId] ツイートの取得" should {
+      "指定したツイートが存在して正常にツイートが取得される" in new WithApplication{
+        val tweetId = 1
+        val req = FakeRequest(GET, s"/tweets/${tweetId}")
+          .withHeaders("Content-Type" -> "application/json")
+
+        val fres = route(req).get
+
+        status(fres) must equalTo(OK)
+        contentType(fres) must beSome("application/json")
+        contentAsString(fres) must contain ("tweet")
+      }
+
+      " 指定したツイートが存在せず、ツイート取得に失敗する" in new WithApplication{
+        val tweetId = 100000
+        val req = FakeRequest(GET, s"/tweets/${tweetId}")
+          .withHeaders("Content-Type" -> "application/json")
+
+        val fres = route(req).get
+
+        status(fres) must equalTo(NOT_FOUND)
+        contentType(fres) must beSome("application/json")
+        contentAsString(fres) must contain ("error")
+      }
 
     }
+    
   }
 }
 
