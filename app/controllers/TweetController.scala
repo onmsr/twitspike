aafb8c3cf67502896d937d1bb7aa6982d0cae56c
onuma <masaru_onuma@dwango.co.jp>
2015-09-03 15:27:39 +0900

    ツイート作成のAPI追加。

--- app/controllers/TweetController.scala
+++ app/controllers/TweetController.scala
@@ -1,6 +1,13 @@
 package jp.co.dwango.twitspike.controllers
 
-import play.api.mvc.Controller
+import jp.co.dwango.twitspike.services.AerospikeService
+import jp.co.dwango.twitspike.services.TweetService
+import jp.co.dwango.twitspike.validations.TweetRequestDataConstraint
+import jp.co.dwango.twitspike.exceptions.TwitSpikeException
+import jp.co.dwango.twitspike.actions.UserAction
+import play.api.libs.json.Json
+import scala.util.control.Exception.allCatch
+import jp.co.dwango.twitspike.exceptions.TwitSpikeException.writes
 
 /**
  * TweetController
@@ -9,7 +16,29 @@ import play.api.mvc.Controller
  */
 class TweetController extends BaseController {
 
-  def post() = TODO
+  def post() = UserAction(parse.json) { implicit request =>
+    (for {
+      sessionKey <- request.sessionKey.toRight(
+        new TwitSpikeException(SESSION_KEY_REQUIRED_ERROR, sessionKeyRequiredErrorMessage)).right
+      user <- request.user.toRight(
+        new TwitSpikeException(AUTH_USER_NOT_FOUND_ERROR, userNotFoundErrorMessage)).right
+      client <- AerospikeService.getClient.right
+      tweet <- getRequestData(TweetRequestDataConstraint.post).right
+      id <- new TweetService(client).create(user.id, tweet.content).right
+      _ <- (allCatch either client.close).right
+    } yield id) match {
+      case Left(e) => {
+        e match {
+          case TwitSpikeException(SESSION_KEY_REQUIRED_ERROR, _) => BadRequest(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case TwitSpikeException(AUTH_USER_NOT_FOUND_ERROR, _) => NotFound(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case TwitSpikeException(VALIDATIONS_ERROR, _) => BadRequest(Json.obj("error" -> Json.toJson(e.asInstanceOf[TwitSpikeException])))
+          case _ => InternalServerError(Json.obj("error" -> internalServerErrorMessage))
+        }
+      }
+      case Right(id) => Created(Json.obj("id" -> id))
+    }
+  }
+
   def delete(tweetId: Long) = TODO
   def get(tweetId: Long) = TODO
 
